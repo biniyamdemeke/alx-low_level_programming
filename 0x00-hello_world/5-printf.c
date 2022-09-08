551aef1b09c2fb118ddb0f1e393be8b28be15c49
biniyamdemeke <biniyamdemeke7@gmail.com>
2022-09-08 12:37:04 -0700

    Task5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,11 @@
-
 #include <stdio.h>
-
 /**
  *main - Prints out a string to stdout.
  *
- *Return: Always 0 if success
+ *Return: Always 0
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");	
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
