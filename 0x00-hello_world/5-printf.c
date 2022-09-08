e71eec704a4a5b4661453fd65d087a88861d8493
biniyamdemeke <biniyamdemeke7@gmail.com>
2022-09-08 12:33:53 -0700

    Task5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,9 @@
 #include <stdio.h>
 
 /**
- *main - prints "with proper grammer, but the outcome is a piece of art, " followed by a new line.
+ *main - Prints out a string to stdout.
  *
- *
- *Return: Always 0.
+ *Return: Always 0 if success
  */
 int main(void)
 {
