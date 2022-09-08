efd0bcbe9a6f6ca0902f1432a4fc025259f369d1
biniyamdemeke <biniyamdemeke7@gmail.com>
2022-09-08 12:16:48 -0700

    Task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,14 @@
+/*
+ * File: 4-puts.c
+ * Auth: Biniyam Demeke
+ */
+
 #include <stdio.h>
+
+/*
+ * main function
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
