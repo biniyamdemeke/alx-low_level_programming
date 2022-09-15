de1b4129d079ce74b9a5fd31400e1d9c9804d416
biniyamdemeke <biniyamdemeke7@gmail.com>
2022-09-15 15:01:48 -0700

    Task10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_triangle - Make a triangle.
+ * @size: parameter used for size.
+ * Return: Always 0.
+ */
+void print_triangle(int size)
+{
+	int height;
+	int width;
+
+	if (size <= 0)
+		_putchar('\n');
+	for (width = 1; width <= size; width++)
+	{
+		for (height = 1; height <= size; height++)
+		{
+			if (height <= (size - width))
+				_putchar(' ');
+			else
+				_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
