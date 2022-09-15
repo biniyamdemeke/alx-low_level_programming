3c66e38d43f1a4b22d5cbc2fd8a2c9d79de9f478
biniyamdemeke <biniyamdemeke7@gmail.com>
2022-09-15 14:19:10 -0700

    Task4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_most_numbers - write a function that prints the numbers from 0-9,
+ * followed by a new line.
+ *
+ * Return: Always 0.
+ */
+void print_most_numbers(void)
+{
+	int c = '0';
+
+	while (c <= '9')
+	{
+		if ((c != '2') && (c != '4'))
+		{
+			_putchar(c);
+		}
+		c++;
+	}
+	_putchar('\n');
+}
