6ce1f5494b721b822f6ed4c9f3d6cfccf7c52659
biniyamdemeke <biniyamdemeke7@gmail.com>
2022-09-19 05:23:45 -0700

    Task5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,26 +6,32 @@
  */
 void rev_string(char *s)
 {
-	int i;
-	int j;
-	int temp;
-	int length;
+	char *start_c, *end_c, c;
+	int i, count;
+	int length = 0;
 
-	while (s[i] != 0)
+	for (i = 0; s[i]; i++)
 	{
-		i++;
+		length++;
 	}
 
-	length = i;
-	i = 0;
-	j = length - 1;
+	count = length;
 
-	while (i < j)
+	start_c = s;
+	end_c = s;
+
+	for (i = 0; i < count - 1; i++)
 	{
-		temp = s[i];
-		s[i] = s[j];
-		s[j] = temp;
-		i++;
-		j--;
+		end_c++;
+	}
+
+	for (i = 0; i < count / 2; i++)
+	{
+		c = *end_c;
+		*end_c = *start_c;
+		*start_c = c;
+
+		start_c++;
+		end_c--;
 	}
 }
