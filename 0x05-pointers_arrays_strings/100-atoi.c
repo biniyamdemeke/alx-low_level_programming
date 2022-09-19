ee7f42657d934f9ec536e0543c3b64fdb5a96f9f
biniyamdemeke <biniyamdemeke7@gmail.com>
2022-09-19 06:06:41 -0700

    Task100

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,40 +1,32 @@
 #include "coding.h"
 /**
- * is_numerical - check if it is a digit
- * @n: Number
- * Return: If is a number, return 1 else return 0
- */
-int is_numerical(unsigned int n)
-{
-	return (n >= '0' &&  n <= '9');
-}
-
-/**
  * _atoi - convert a string to an integer
  * @s: String
  * aReturn: Return the num
  */
 int _atoi(char *s)
 {
-	unsigned int number, i;
-	int sign;
+	short boolean;
+	int i, minus, result;
 
-	sign = 1;
-	number = 0;
+	i = minus = result = boolean = 0;
+	minus = -1;
 
-	for (i = 0; s[i] != '\0'; i++)
+	while (s[i] != '\0')
 	{
-		if (is_numerical(s[i]))
-		{
-			number = (s[i] - 48) + number * 10;
+		if (s[i] == '-')
+			minus *= -1;
 
-			if (s[i + 1] == ' ')
-				break;
-		}
-		else if (s[i] == '-')
+		if (s[i] >= '0' && s[i] <= '9')
 		{
-			sign *= -1;
+			result *= 10;
+			result -= (s[i] - '0');
+			boolean = 1;
 		}
+		else if (boolean == 1)
+			break;
+		i++;
 	}
-	return (number * sign);
+	result *= minus;
+	return (result);
 }
