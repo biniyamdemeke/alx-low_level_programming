d15074335b93e0423744eb9f4cf99041c0bbcd9d
biniyamdemeke <biniyamdemeke7@gmail.com>
2022-09-19 05:46:51 -0700

    Task100

--- 0x05-pointers_arrays_strings/coding.h
+++ 0x05-pointers_arrays_strings/coding.h
@@ -1,5 +1,5 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef CODING_H
+
 void reset_to_98(int *n);
 int _putchar(char c);
 void swap_int(int *a, int *b);
@@ -11,4 +11,7 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+
+#define CODING_H
 #endif
