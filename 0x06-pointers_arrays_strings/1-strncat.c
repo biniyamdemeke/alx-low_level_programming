742989c3727e7980811471f00b5097575cc65b42
biniyamdemeke <biniyamdemeke7@gmail.com>
2022-09-21 11:46:07 -0700

    Task1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -6,7 +6,7 @@
  * @n: the number of bytes to use from src.
  * Return: string.
  */
-char * _strncat(char * dest, char * src, int n)
+char *_strncat(char * dest, char * src, int n)
 {
 	int i = 0, k = 0;
 	
