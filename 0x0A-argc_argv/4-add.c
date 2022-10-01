90c00cb85e78c2f5ca664e0906c3ddadd447718e
biniyamdemeke <biniyamdemeke7@gmail.com>
2022-10-01 11:55:44 -0700

    Task4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,39 +13,21 @@
  *
  *      */
 int main(int argc, char **argv)
-
 {
+	int a, b, addition;
 
-		int a, b, addition;
-
-
-
-			for (a = 1; a < argc; a++)
-
-					{
-
-								for (b = 0; argv[a][b] != '\0'; b++)
-
-											{
-
-															if (!isdigit(argv[a][b]))
-
-																			{
-
-																								printf("Error\n");
-
-																												return (1);
-
-																															}
-
-																	}
-
-										addition = addition + atoi(argv[a]);
-
-											}
-
-				printf("%i\n", addition);
-
-					return (0);
-
+	for (a = 1; a < argc; a++)
+	{
+		for (b = 0; argv[a][b] != '\0'; b++)
+		{
+			if (!isdigit(argv[a][b]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		addition = addition + atoi(argv[a]);
+	}
+	printf("%i\n", addition);
+	return (0);
 }
