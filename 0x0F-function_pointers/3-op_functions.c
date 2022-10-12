b5d9cca4a0c745d493064908b4a1331ef5e138d0
biniyamdemeke <biniyamdemeke7@gmail.com>
2022-10-12 08:54:40 -0700

    Task3

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,23 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * op_add - adds a and b
+ * @a: first number
+ * @b: second number
+ *
+ * Return: sum of a and b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtracts b from a
+ * @a: first number
+ * @b: second number
+ *
+ * Return: difference of a and b
+ */
+
